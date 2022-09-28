6b9335b5435673fc96a6faa4594f1f4af5e5c02d
unknown <188r1a1202@gmail.com>
2022-09-28 12:02:17 +0530

    MY AppS

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/<angular-app-heroku>'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/<Angular-app-heroku>/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,11 +22,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/<angular-app-heroku>'));
+app.use(express.static(__dirname + '/dist/<ANGULAR-APP-HEROKU>'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/<Angular-app-heroku>/'}),
-);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/<ANGULAR-APP-HEROKU>/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
